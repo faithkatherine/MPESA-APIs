7a5e0e030bd0fd318ce677df3ee8f9cf81aff22c
faithkatherine <faithcathy12@gmail.com>
2020-08-30 22:15:04 +0300

    my first commit

--- index.php
+++ index.php
@@ -11,6 +11,6 @@
 	
 	$curl_response = curl_exec($curl);
 	
-	echo"<pre> Access Token" .json_decode($curl_response)->access_token. "</pre>";
+	echo"<pre> Access Token" .json_decode($curl_response)->$access_token. "</pre>";
 	
 	?>
--- register_url.php
+++ register_url.php
@@ -1,26 +1,17 @@
 <?php
-
-	$consumerkey = "3ocIH6gitgAGQGa8OChkP5EwgwqzCYJ4";
-	$consumersecret = "KAMriE9VEHqp9GQB";
-
-	$headers = ['Content-Type: applications/json; char-set = UTF-8'];
-
 	$url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
-
-	$curl = curl_init($url);
-	curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-
+	
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, $url);
+	$credentials = base64_encode('3ocIH6gitgAGQGa8OChkP5EwgwqzCYJ4:KAMriE9VEHqp9GQB');
+	curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Basic '.$credentials)); //setting a custom header
 	curl_setopt($curl, CURLOPT_HEADER, false);
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($curl, CURLOPT_USERPWD, $consumerkey.':'.$consumersecret);
-
-	$result = curl_exec($curl);
-	$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-	$result = json_decode($result);
-
-	$access_token = $result->access;
-
-	echo $access_token;
+	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+	
+	$curl_response = curl_exec($curl);
+	
+	echo"<pre> Access Token" .json_decode($curl_response)->$access_token. "</pre>";
 
 	$url = 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl';
 	
